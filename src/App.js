90f496f2878976feda1ef82d80ed411b48bb88c1
Maxgvn <46270859+maxgvn@users.noreply.github.com>
2019-04-20 18:26:11 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    originalUrl: "",
+    arrayLinks: [],
+    isLoaded: false
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://short-url-max-gavanon.herokuapp.com/api/links"
+      );
+      this.setState({ arrayLinks: response.data, isLoaded: true });
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="headerTop">
+          <div className="centerMain">
+            <div className="title-div">
+              <h1> Simplify your links</h1>
+            </div>
+            <div>
+              <form className="input-form">
+                <input
+                  className="form-control formleft"
+                  type="text"
+                  placeholder="Votre URL à raccourcir - avec http préfixe"
+                  value={this.state.originalUrl}
+                  onChange={e => this.setState({ originalUrl: e.target.value })}
+                />
+
+                <input
+                  className="btn-default formright"
+                  value="SHORTEN URL"
+                  type="button"
+                  onClick={() =>
+                    axios.post(
+                      "https://short-url-max-gavanon.herokuapp.com/api/shorten",
+                      { originalUrl: this.state.originalUrl }
+                    )
+                  }
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="bodyMain">
+          <div className="centerMain">
+            <ul className="list-group list-group-flush">
+              <li className="linklist list-group-item grey">
+                <strong> Lien d'origine</strong>
+                <strong> Lien raccourci</strong>
+              </li>
+              {this.state.arrayLinks.map((link, index) => {
+                return (
+                  <li key={index} className="linklist list-group-item">
+                    <div> {link.originalUrl} </div>
+                    <div> {link.shortUrl} </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
