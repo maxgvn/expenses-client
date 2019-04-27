c8782fe87595976391fe6b650337825df757c050
Maxgvn <46270859+maxgvn@users.noreply.github.com>
2019-04-27 18:02:01 +0200

    "css 2704"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 
 import "./App.css";
 
@@ -24,52 +23,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="headerTop">
-          <div className="centerMain">
-            <div className="title-div">
-              <h1> Simplify your links</h1>
+        <div className="four-main">
+          <div className="left-col">
+            <div className="main-boxes">
+              <h1>Users</h1>
             </div>
-            <div>
-              <form className="input-form">
-                <input
-                  className="form-control formleft"
-                  type="text"
-                  placeholder="Votre URL à raccourcir - avec http préfixe"
-                  value={this.state.originalUrl}
-                  onChange={e => this.setState({ originalUrl: e.target.value })}
-                />
-
-                <input
-                  className="btn-default formright"
-                  value="SHORTEN URL"
-                  type="button"
-                  onClick={() =>
-                    axios.post(
-                      "https://short-url-max-gavanon.herokuapp.com/api/shorten",
-                      { originalUrl: this.state.originalUrl }
-                    )
-                  }
-                />
-              </form>
+            <div className="main-boxes">
+              <h1>New User</h1>
             </div>
           </div>
-        </div>
-        <div className="bodyMain">
-          <div className="centerMain">
-            <ul className="list-group list-group-flush">
-              <li className="linklist list-group-item grey">
-                <strong> Lien d'origine</strong>
-                <strong> Lien raccourci</strong>
-              </li>
-              {this.state.arrayLinks.map((link, index) => {
-                return (
-                  <li key={index} className="linklist list-group-item">
-                    <div> {link.originalUrl} </div>
-                    <div> {link.shortUrl} </div>
-                  </li>
-                );
-              })}
-            </ul>
+          <div className="right-col">
+            <div className="main-boxes">
+              <h1>New User</h1>
+            </div>
+            <div className="main-boxes">
+              <h1>New User</h1>
+            </div>
           </div>
         </div>
       </div>
