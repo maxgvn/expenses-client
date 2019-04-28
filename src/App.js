95df4849b7ba3d3dd7bca75c0dfa8e7560aab0c7
Maxgvn <46270859+maxgvn@users.noreply.github.com>
2019-04-28 17:40:07 +0200

    "afternoon 2404"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    originalUrl: "",
-    arrayLinks: [],
-    isLoaded: false
+    users: [],
+    userName: "",
+    expenses: [],
+    expenseDescription: "",
+    expenseAmount: "",
+    expenseUser: ""
+  };
+  async componentDidMount() {
+    try {
+      this.updateInfo();
+    } catch (error) {
+      alert("Une erreur est survenue");
+    }
+  }
+
+  updateInfo = async () => {
+    // On demande la liste à jour des tâches au serveur
+    const usersGet = await axios.get("http://localhost:3000/api/users");
+    const expensesGet = await axios.get("http://localhost:3000/api/expenses");
+    this.setState({
+      users: usersGet.data,
+      expenses: expensesGet.data
+    });
   };
 
-  // async componentDidMount() {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://short-url-max-gavanon.herokuapp.com/api/links"
-  //     );
-  //     this.setState({ arrayLinks: response.data, isLoaded: true });
-  //   } catch (error) {
-  //     alert(error);
-  //   }
-  // }
+  handleChange = event => {
+    const name = event.target.name;
+
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmitUser = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/usercreate", {
+        name: this.state.userName
+      });
+
+      this.updateInfo();
+    } catch (error) {
+      alert("Une erreur est survenue");
+    }
+  };
+
+  handleSubmitExpense = async () => {
+    try {
+      await axios.post("http://localhost:3000/api/expensecreate", {
+        description: this.state.expenseDescription,
+        amount: this.state.expenseAmount,
+        user: this.state.expenseUser
+      });
+
+      this.updateInfo();
+    } catch (error) {
+      alert("Une erreur est survenue");
+    }
+  };
 
   render() {
     return (
@@ -27,17 +73,95 @@ class App extends Component {
           <div className="left-col">
             <div className="main-boxes">
               <h1>Users</h1>
+              <ul className="tasks-list">
+                {this.state.users.map((user, index) => {
+                  return (
+                    <li key={index}>
+                      {user.name} - €{user.expensestotal}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
             <div className="main-boxes">
               <h1>New User</h1>
+              {console.log(this.state, "first state check")}
+              <form onSubmit={this.handleSubmitUser}>
+                <input
+                  type="text"
+                  name="userName"
+                  placeholder="User name"
+                  value={this.state.userName}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <br />
+                <input type="submit" value="ADD USER" />
+              </form>
             </div>
           </div>
           <div className="right-col">
             <div className="main-boxes">
-              <h1>New User</h1>
+              <h1>Expenses</h1>
+              <ul className="expense-list">
+                {this.state.expenses.map((expense, index) => {
+                  return (
+                    <li key={index}>
+                      <span
+                        onClick={async () => {
+                          await axios.post(
+                            "http://localhost:3000/api/deleteexpense?id=" +
+                              expense._id
+                          );
+                          this.updateInfo();
+                        }}
+                      >
+                        <p>
+                          {expense.Description} - {expense.User.name} - €
+                          {expense.Amount}
+                        </p>
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
             <div className="main-boxes">
-              <h1>New User</h1>
+              <h1>New expense</h1>
+              <form onSubmit={this.handleSubmitExpense}>
+                <input
+                  type="text"
+                  name="expenseDescription"
+                  placeholder="Description"
+                  value={this.state.expenseDescription}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <input
+                  type="text"
+                  name="expenseAmount"
+                  placeholder="Amount"
+                  value={this.state.expenseAmount}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <select
+                  value={this.state.value}
+                  name="expenseUser"
+                  onChange={this.handleChange}
+                >
+                  {this.state.users.map((user, index) => {
+                    return (
+                      <option value={user._id} key={index}>
+                        {user.name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <br />
+                <br />
+                <input type="submit" value="ADD EXPENSE" />
+              </form>
             </div>
           </div>
         </div>
